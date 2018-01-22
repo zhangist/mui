5187364f0f239a0f24c3394bc6f73038982209aa
zhangist <zhangist@163.com>
2018-01-22 18:57:33 +0800

    dom-helpers add types

--- src/@types/dom-helpers.d.ts
+++ src/@types/dom-helpers.d.ts
@@ -1,6 +1,6 @@
-declare module 'dom-helpers/query/contains' {
-  export = contains
-  function contains(arg: any, arg1: any): boolean
+declare module 'dom-helpers/activeElement' {
+  export = activeElement
+  function activeElement(arg?: any): any
 }
 
 declare module 'dom-helpers/ownerDocument' {
@@ -8,12 +8,17 @@ declare module 'dom-helpers/ownerDocument' {
   function ownerDocument(arg: any): any
 }
 
+declare module 'dom-helpers/query/contains' {
+  export = contains
+  function contains(arg: any, arg1: any): boolean
+}
+
 declare module 'dom-helpers/util/inDOM' {
   export = inDOM
   function inDOM(arg: any): any
 }
 
-declare module 'dom-helpers/activeElement' {
-  export = activeElement
-  function activeElement(arg?: any): any
+declare module 'dom-helpers/util/scrollbarSize' {
+  export = scrollbarSize
+  function scrollbarSize(): any
 }
